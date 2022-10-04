82fae31ead63a63356fe2af4b0379fda945d4d96
Sahil <leviack629@gmail.com>
2022-10-04 21:32:39 +0530

    added function

--- src/Sum.java
+++ src/Sum.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
 
-public class Fun {
+public class Sum {
     public static void main(String[] args) {
+        sum();
+    }
+    static void sum() {
         Scanner in = new Scanner(System.in);
-
         System.out.print("Enter num1: ");
         int num1 = in.nextInt();
         System.out.print("Enter num2: ");
